1d6ae6bbd62f1cc05513af3cd4fb301eda9a9ae1
Excel Iyoha <exceliyoha53@gmail.com>
2026-03-09 00:51:51 +0100

    Day 2: Upgraded extractor to save data as CSV and ignored CSV files in git

--- Month_01_Automation/Week_01_Basics/extractor.py
+++ Month_01_Automation/Week_01_Basics/extractor.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,7 +12,7 @@ class LeadExtractor:
         self.session = requests.Session()  # A session keeps our connection alive and holds cookies
 
     def fetch_page(self):
-        print(f"Fetching: {self.base_url}")
+        print(f"\nFetching: {self.base_url}")
         try:
             response = self.session.get(self.base_url, headers=self.headers, timeout=10)
             response.raise_for_status()  # 404:page not found|403:forbidden|500: server error
@@ -22,27 +23,40 @@ class LeadExtractor:
 
     def extract_data(self):
         html_content = self.fetch_page()
-
         if not html_content:
-            print("No data to parse.")
-            return
+            return []  # Return an empty list if t he fetching failed
 
         print("Parsing the data...\n")
-
         soup = BeautifulSoup(html_content, "html.parser")
+
+        extracted_leads = []
 
         articles = soup.find_all("tr", class_="athing", limit=5)
         for article in articles:
             title_tag = soup.find("span", class_="titleline").find("a")
             title = title_tag.getText()
             link = title_tag.get("href")
-            print(f"Title: {title}")
-            print(f"Link: {link}")
-            print("-" * 40)
+
+            lead_data = {"Title": title, "Link": link}
+            extracted_leads.append(lead_data)
+        return extracted_leads
+
+    def save_to_csv(self, data, filename="leads.csv"):
+        if not data:
+            print("No data to save!")
+            return
+        print(f"Saving {len(data)} leads to {filename}...")
+        column_headers = ["Title", "Link"]
+        with open(filename, mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=column_headers)
+            writer.writeheader()
+            writer.writerows(data)
+        print("Save Complete!!!")
 
 
 if __name__ == "__main__":
     target_site = "https://news.ycombinator.com/"
     my_scraper = LeadExtractor(target_site)
 
-    my_scraper.extract_data()
+    scraped_data = my_scraper.extract_data()
+    my_scraper.save_to_csv(scraped_data, "hacker_news_leads.csv")
